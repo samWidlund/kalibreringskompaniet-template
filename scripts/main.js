b7affde5d67eaaa4fdbb1cb18d2b0a365e5d738a
samWidlund <sam.wid415@gmail.com>
2026-01-24 22:36:18 +0100

    contact page + form

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,10 @@ landscapeVideo.load();
 portraitVideo.defaultPlaybackRate = videoSpeed;
 portraitVideo.playbackRate = videoSpeed;
 portraitVideo.load();
+
+// contact form
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Tack för ditt meddelande! Vi återkommer snart.');
+    this.reset();
+});
